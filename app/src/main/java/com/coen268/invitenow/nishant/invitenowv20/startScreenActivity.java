0bfa7b347492ea9b80c308188918a71a202aac59
Nishant Phatangare <nishant1914@gmail.com>
2015-02-21 02:53:29 -0800

    user registration done Nis

--- app/src/main/java/com/coen268/invitenow/nishant/invitenowv20/startScreenActivity.java
+++ app/src/main/java/com/coen268/invitenow/nishant/invitenowv20/startScreenActivity.java
@@ -1,25 +1,49 @@
 package com.coen268.invitenow.nishant.invitenowv20;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.ParseQuery;
+import com.parse.ParseException;
+
+import java.util.List;
 
 
 public class startScreenActivity extends ActionBarActivity {
 
-
+    EditText username, password;
+    Button signUp;
+    String usernameString, passwordString;
+    String usernameCheck;
+    String passwordCheck;
+    int clickedUserID;
+    boolean tableInMemory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        username = (EditText)findViewById(R.id.usernameEditText);
+        password = (EditText)findViewById(R.id.passwordEditText);
 
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
@@ -30,18 +54,214 @@ public class startScreenActivity extends ActionBarActivity {
         testObject.put("foo", "bar1");
         testObject.saveInBackground();
         */
+/*
+        SQLiteDatabase db = new userDB(this).getWritableDatabase();
 
+        tableInMemory = isTableExists(db,userDB.DATABASE_TABLE);
 
-/*      Set Start screen Image Here. Random image is set at this moment.
-        ImageView startScreenImage = (ImageView) findViewById(R.id.startScreenImage);
-        startScreenImage.setImageResource(R.drawable.panda);
-*/
+        if(!tableInMemory) {
+
+            saveUserToSQLite("sample user", "sample password");
+        }*/
+
+        readFromDB();
+
+        ParseUser.logInInBackground(usernameCheck, passwordCheck, new LogInCallback() {
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    // Hooray! The user is logged in.
+                    Toast.makeText(getApplicationContext(), "Login Success",
+                            Toast.LENGTH_SHORT).show();
+
+                    Intent enterSendInvites2 = new Intent(startScreenActivity.this, SendInvitesActivity.class);
+                    startActivity(enterSendInvites2);
+
+                } else {
+                    // Signup failed. Look at the ParseException to see what happened.
+                    Toast.makeText(getApplicationContext(), passwordCheck,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+       /* ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+
+        query.whereEqualTo("username", usernameCheck);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (object == null) {
+                    //Log.d("score", "The getFirst request failed.");
+                    Toast.makeText(getApplicationContext(), usernameCheck,
+                            Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //Log.d("score", "Retrieved the object.");
+                    Toast.makeText(getApplicationContext(), "Login Success",
+                            Toast.LENGTH_SHORT).show();
+
+                    Intent enterSendInvites2 = new Intent(startScreenActivity.this, SendInvitesActivity.class);
+                    startActivity(enterSendInvites2);
+                }
+            }
+        });
+        */
+        /*
+        query.findInBackground(new FindCallback<ParseObject>() {
+
+            public void done(List<ParseObject> scoreList, ParseException e) {
+                if (e == null) {
+                    Log.d("score", "Retrieved " + scoreList.size() + " scores");
+                } else {
+                    Log.d("score", "Error: " + e.getMessage());
+                }
+            }
+        });
+        */
+
     }
+
+    boolean isTableExists(SQLiteDatabase db, String tableName)
+    {
+        if (tableName == null || db == null || !db.isOpen())
+        {
+            return false;
+        }
+        Cursor cursor = db.rawQuery("SELECT COUNT(*) FROM sqlite_master WHERE type = ? AND name = ?", new String[] {"table", tableName});
+        if (!cursor.moveToFirst())
+        {
+            return false;
+        }
+        int count = cursor.getInt(0);
+        cursor.close();
+        return count > 0;
+    }
+
+    public void doLogin(View view)
+    {
+        usernameString = username.getText().toString();
+        passwordString = password.getText().toString();
+
+        ParseUser.logInInBackground(usernameString, passwordString, new LogInCallback() {
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    // Hooray! The user is logged in.
+                    Toast.makeText(getApplicationContext(), "Login Success",
+                            Toast.LENGTH_SHORT).show();
+                    saveUserToSQLite(usernameString,passwordString);
+                    Intent enterSendInvites2 = new Intent(startScreenActivity.this, SendInvitesActivity.class);
+                    startActivity(enterSendInvites2);
+
+                } else {
+                    // Signup failed. Look at the ParseException to see what happened.
+                    Toast.makeText(getApplicationContext(), "Username or Password incorrect",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public void doSignUp(View view)
+    {
+
+        usernameString = username.getText().toString();
+        passwordString = password.getText().toString();
+
+        if(usernameString != null && passwordString != null) {
+            ParseUser user = new ParseUser();
+            user.setUsername(usernameString);
+            user.setPassword(passwordString);
+            //user.setEmail("email@example.com");
+
+                // other fields can be set just like with ParseObject
+                //user.put("phone", "650-253-0000");
+
+            user.signUpInBackground(new com.parse.SignUpCallback() {
+                public void done(ParseException e) {
+                    if (e == null) {
+                        // Hooray! Let them use the app now.
+                        Toast.makeText(getApplicationContext(), "Sign Up Successful",
+                                Toast.LENGTH_SHORT).show();
+
+                        saveUserToSQLite(usernameString,passwordString);
+
+                        Intent enterSendInvites = new Intent(startScreenActivity.this, SendInvitesActivity.class);
+                        startActivity(enterSendInvites);
+
+
+                    } else {
+                        // Sign up didn't succeed. Look at the ParseException
+                        // to figure out what went wrong
+                        Toast.makeText(getApplicationContext(), "Something went wrong",
+                                Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Something went wrong 2",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void saveUserToSQLite(String usernameDB, String passwordDB) {
+        SQLiteDatabase db = new userDB(this).getWritableDatabase();
+        ContentValues newValues = new ContentValues();
+        newValues.put(userDB.COLUMN_USERNAME, usernameDB);
+        newValues.put(userDB.COLUMN_PASSWORD, passwordDB);
+
+        //-----For Debug-----//
+        // newValues.put(NotesDB.NAME_COLUMN, path);
+        //newValues.put(NotesDB.FILE_PATH_COLUMN, caption);
+        //-----For Debug-----//
+
+        db.insert(userDB.DATABASE_TABLE, null, newValues);
+        Toast.makeText(getApplicationContext(), "Saved in DataBase", Toast.LENGTH_SHORT).show();
+    }
+
+    private void readFromDB() {
+        SQLiteDatabase db = new userDB(this).getWritableDatabase();
+        /*
+        String where = null;
+        String whereArgs[] = null;
+        String groupBy = null;
+        String having = null;
+        String order = null;
+        String[] resultColumns = {userDB.COLUMN_ID, userDB.COLUMN_USERNAME, userDB.COLUMN_PASSWORD};
+        Cursor cursor = db.query(userDB.DATABASE_TABLE, resultColumns, where, whereArgs,
+                groupBy, having, order);
+        while (cursor.moveToNext()) {
+            int idInDB = cursor.getInt(cursor.getColumnIndex(userDB.COLUMN_ID));
+            if (clickedUserID + 1 == idInDB) {
+                usernameCheck = cursor.getString(cursor.getColumnIndex(userDB.COLUMN_USERNAME));
+                passwordCheck = cursor.getString(cursor.getColumnIndex(userDB.COLUMN_PASSWORD));
+                break;
+            }
+
+        }
+        */
+        Cursor cursor = db.rawQuery("Select * from  User", null);
+        int number = cursor.getCount();
+        if(number == 0)
+        {
+            saveUserToSQLite("sample user", "sample password");
+        }
+
+            cursor.moveToLast();
+            usernameCheck = cursor.getString(cursor.getColumnIndex(userDB.COLUMN_USERNAME));
+            passwordCheck = cursor.getString(cursor.getColumnIndex(userDB.COLUMN_PASSWORD));
+
+    }
+
 
     public void enterApplication(View view)
     {
-        Intent enterSendInvites = new Intent(this, SendInvitesActivity.class);
-        startActivity(enterSendInvites);
+        //Intent enterSendInvites = new Intent(this, SendInvitesActivity.class);
+        //startActivity(enterSendInvites);
 
 /*      Ability to change the Start Screen Image on tap. Random image is set at this moment.
         ImageView startScreenImage = (ImageView) findViewById(R.id.startScreenImage);
---
+++ app/src/main/java/com/coen268/invitenow/nishant/invitenowv20/userDB.java
@@ -0,0 +1,53 @@
+package com.coen268.invitenow.nishant.invitenowv20;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Nishant on 2/21/2015.
+ */
+public class userDB extends SQLiteOpenHelper {
+
+    /* Schema and Contract */
+    public static final String COLUMN_ID = "ID";
+    public static final String COLUMN_USERNAME = "USERNAME";
+    public static final String COLUMN_PASSWORD = "PASSWORD";
+
+    public static final String DATABASE_TABLE = "User";
+    public static final int DATABASE_VERSION = 1;
+    /* Schema and Contract */
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + DATABASE_TABLE;
+
+
+    private static final String DATABASE_CREATE = String.format(
+            "CREATE TABLE %s (" +
+                    "  %s integer primary key autoincrement, " +
+                    "  %s text," +
+                    "  %s text)",
+            DATABASE_TABLE, COLUMN_ID, COLUMN_USERNAME, COLUMN_PASSWORD);
+
+    public userDB(Context context) {
+        super(context, DATABASE_TABLE, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
