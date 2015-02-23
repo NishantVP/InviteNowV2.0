4804e78f57d798cfde92b8bebae7d261f22c4bcd
Prasad Deshpande <prasad.deshpande65@yahoo.com>
2015-02-22 18:55:09 -0800

    Commit for contacts single and groups by PD

--- app/src/main/java/com/coen268/invitenow/nishant/invitenowv20/SelectFromGroupsActivity.java
+++ app/src/main/java/com/coen268/invitenow/nishant/invitenowv20/SelectFromGroupsActivity.java
@@ -1,65 +1,194 @@
 package com.coen268.invitenow.nishant.invitenowv20;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
+@SuppressWarnings("ALL")
 public class SelectFromGroupsActivity extends ActionBarActivity {
 
-    private String[] groupNames;
-    private int[] groupImages;
-    private ListView groupList;
-    private groupListAdapter groupListAdapter;
+    ListView listView;
+    EditText editText;
+    Cursor cursor;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_from_groups);
+        Intent intent = getIntent();
 
-        groupImages = new int[] {R.drawable.elephant,R.drawable.elephant,R.drawable.elephant,
-                R.drawable.elephant};
-        groupNames = new String[] {" Group 1 "," Group 2", " Group 3"," Group 4"};
-        groupList = (ListView) findViewById(R.id.listView3);
-        groupListAdapter = new groupListAdapter(this, groupNames, groupImages);
+        context = this;
+        listView = (ListView)findViewById(R.id.mylist);
+        editText = (EditText)findViewById(R.id.textview);
 
-        groupList.setAdapter(groupListAdapter);
+        try {
+            // Create an array of Strings, for List
+            ArrayAdapter<Model> adapter = new InteractiveArrayAdapter(this, getModel());
 
+            // Assign adapter to ListView
+            listView.setAdapter(adapter);
 
-        groupList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        // On Click CheckBox will be checked
-
-                        break;
-
-                    case 1:
-                        // On Click CheckBox will be checked
-
-                        break;
-                    case 2:
-                        // On Click CheckBox will be checked
-                        break;
-
-                    case 3:
-                        // On Click CheckBox will be checked
-
-                        break;
+            listView.setOnItemClickListener(new OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    Toast.makeText(getApplicationContext(),"Click ListItem Number " + position, Toast.LENGTH_LONG).show();
                 }
-            }
-        });
-
-
+            });
+        }catch (Exception e){
+            Log.d("*****exception",e.getMessage());
+        }
     }
 
+    private List<Model> getModel(){
+
+        List<Model> list = new ArrayList<Model>();
+
+        try{
+            ContentResolver cr = getContentResolver();
+
+            cursor = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+
+            // changing this :: ContactsContract.CommonDataKinds.Phone.CONTENT_URI  to this :: ContactsContract.Groups.CONTENT_URI
+
+            cursor.moveToFirst();
+            if(cursor.moveToFirst()){
+                while(cursor.moveToNext()) {
+
+                     String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                     String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                     String s = name + "\n" + number;
+                 //   String s = cursor.getString(cursor.getColumnIndex(ContactsContract.Groups.TITLE)); // this is the query to return the groups.
+                  if(!list.contains(s)){
+
+                    list.add(get(s));
+                    s = null;
+                  }
+                }
+            }
+        }catch (Exception e){
+            Log.d("********** error in Contact read: ",""+e.getMessage());
+        }
+        return list;
+    }
+
+    private Model get(String s) {
+        return new Model(s);
+    }
+
+    public void onClick(View v){
+        switch(v.getId()){
+
+            case R.id.mybutton:
+                String s="";
+                for(int i=0; i<InteractiveArrayAdapter.list.size();i++){
+                    if(InteractiveArrayAdapter.list.get(i).isSelected()){
+
+                        s = s + i + " ";
+                    }
+                }
+                String s1 = null;
+                s1 = editText.getText().toString();
+
+                // Check the edittext is empty or not
+                if(s1.equals("")){
+                    Toast.makeText(SelectFromGroupsActivity.this, "Please Enter Any Text", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Cursor grpCursor;
+                String[] GROUP_PROJECTION = new String[]{ContactsContract.Groups._ID,ContactsContract.Groups.TITLE};
+                grpCursor = this.managedQuery(ContactsContract.Groups.CONTENT_URI, GROUP_PROJECTION,ContactsContract.Groups.TITLE + "=?",new String[]{s1},ContactsContract.Groups.TITLE + " ASC");
+                Log.d("**** Here Counts: ", " **" +grpCursor.getCount());
+
+                if(grpCursor.getCount()>0){
+                    Toast.makeText(SelectFromGroupsActivity.this,"Group is already present",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else{
+                    Toast.makeText(SelectFromGroupsActivity.this,"Not available",Toast.LENGTH_SHORT).show();
+
+                    try{
+                        ContentValues groupValues;
+                        ContentResolver cr = this.getContentResolver();
+                        groupValues = new ContentValues();
+                        groupValues.put(ContactsContract.Groups.TITLE,s1);
+                        cr.insert(ContactsContract.Groups.CONTENT_URI,groupValues);
+                        Log.d("######### Group Creation Finished: ", "##### Success");
+
+                    }catch (Exception e){
+                        Log.d("***** Exception : ", " " +e.getMessage());
+                    }
+                    Toast.makeText(SelectFromGroupsActivity.this,"Created Successfully",Toast.LENGTH_SHORT).show();
+                }
+
+                grpCursor.close();
+                grpCursor = null;
+
+
+                Log.d(" ******* Contacts add to Groups ...","**** Fine");
+
+                String groupID = null;
+                Cursor  getGroupID_Cursor = null;
+                getGroupID_Cursor = this.managedQuery(ContactsContract.Groups.CONTENT_URI, GROUP_PROJECTION,ContactsContract.Groups.TITLE + "=?", new String[]{s1},null);
+                Log.d("****** Empty cursor size: ", "** "+getGroupID_Cursor.getCount());
+                getGroupID_Cursor.moveToFirst();
+                Log.d("**** Group ID is: ", "** "+groupID);
+
+                getGroupID_Cursor.close();
+                getGroupID_Cursor = null;
+
+
+                for(int i=0; i<InteractiveArrayAdapter.list.size();i++){
+                    if(InteractiveArrayAdapter.list.get(i).isSelected()){
+                        cursor.moveToPosition(i);
+                        String contactID = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID));
+
+                        long contact = Long.parseLong(contactID);
+                        long group = Long.parseLong(groupID);
+
+                        addToGroup(contact,group);
+
+                        String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                        Log.d("***** Contact Added : "," *: " +name);
+                        Toast.makeText(SelectFromGroupsActivity.this,name+ "added successfully", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+
+                break;
+        }
+    }
+
+    public Uri addToGroup(long personID,long groupID) {
+        ContentValues values = new ContentValues();
+        values.put(ContactsContract.CommonDataKinds.GroupMembership.RAW_CONTACT_ID,personID);
+        values.put(ContactsContract.CommonDataKinds.GroupMembership.GROUP_ROW_ID, groupID);
+        values.put(ContactsContract.CommonDataKinds.GroupMembership.MIMETYPE,ContactsContract.CommonDataKinds.GroupMembership.CONTENT_ITEM_TYPE);
+
+        return this.context.getContentResolver().insert(ContactsContract.Data.CONTENT_URI,values);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
